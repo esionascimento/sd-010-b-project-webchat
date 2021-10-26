b3a0cadf9b0ab3b642e3ecda657c514e12a2f71e
Joao Vanelli <vanellijoao@gmail.com>
2021-10-26 17:17:58 -0300

    updating server.js with socket.io

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
-const cors = require('cors');
+const moment = require('moment');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 const httpServer = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'oi',
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    method: ['GET', 'POST'],
+  },
+});
+
+app.get('/', (_req, res) => {
+  res.render(`${__dirname}/views/index.ejs`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', ({
+    chatMessage,
+    nickname,
+  }) => {
+    const today = new Date().toLocaleString().replace(/\//g, '-');
+    const test = moment().format();
+    console.log(test);
+    io.emit('message', `${today} - ${nickname}: ${chatMessage}`);
   });
 });
 
-httpServer.listen(3001, () => console.log('listening on port 3000'));
+httpServer.listen(PORT, () => console.log(`listening on port ${PORT}`));
