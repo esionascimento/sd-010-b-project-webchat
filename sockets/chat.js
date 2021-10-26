49a828fe1d48be199787d997da909d66e8f76fa7
Misael Bueno <misa.hbueno@gmail.com>
2021-10-26 18:32:41 -0300

    req1 fixed

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,6 +1,7 @@
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', (message) => {
     const { chatMessage, nickname } = message;
+    // fonte: https://blog.betrybe.com/javascript/javascript-date-format/
     const data = new Date();
     const dataForm = (data.getDate(), '-', ((data.getMonth() + 1)), '-', (data.getFullYear()));
     const time = (data.getHours(), ':', ((data.getMinutes() + 1)), ':', (data.getSeconds()));
