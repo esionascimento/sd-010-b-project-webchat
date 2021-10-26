4cdaedb51bf5a911218a9dab494beca61f5ff5f9
denis-rossati <denis.rossatiramos@gmail.com>
2021-10-26 17:15:05 -0300

    finalizando requisito 2

--- src/view/js/script.js
+++ src/view/js/script.js
@@ -2,11 +2,10 @@ const socket = window.io();
 
 const sendButton = document.querySelector('#sendButton');
 const messageInput = document.querySelector('#messageInput');
-const nicknameDisplay = document.querySelector('#userNickname');
-
-const isNewUser = () => {
-
-};
+const nicknameInput = document.querySelector('#nicknameInput');
+const chooseNickname = document.querySelector('#chooseNickname');
+const userNickname = document.querySelector('#userNickname');
+const messages = document.querySelector('#messages');
 
 const generateNickname = () => {
   let id = '';
@@ -19,19 +18,25 @@ const generateNickname = () => {
   return id;
 };
 
-const olderOrNewerUser = () => {
-  const sessionNickname = sessionStorage
-};
+let nickname = generateNickname();
+
+chooseNickname.addEventListener('click', () => {
+  nickname = nicknameInput.value;
+  userNickname.textContent = nickname;
+});
 
 sendButton.addEventListener('click', () => {
   const chatMessage = messageInput.value;
   socket.emit('message', { chatMessage, nickname });
 });
 
-socket.on('message', (message) => {
-
+socket.on('message', (receivedMessage) => {
+  const message = document.createElement('li');
+  message.textContent = receivedMessage;
+  message.setAttribute('data-testid', 'message');
+  messages.appendChild(message);
 });
 
 window.onload = () => {
-
+  userNickname.textContent = nickname;
 };
