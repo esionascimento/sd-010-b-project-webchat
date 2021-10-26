42f5c3f8e6ade9d1c87b86c5af834977dae3aacc
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-10-26 16:17:51 -0300

    iniciando servidor

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3000;
+
+app.use(cors());
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`, 
+    methods: ['GET', 'POST'],
+  } });
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/chat/index.html'));
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
