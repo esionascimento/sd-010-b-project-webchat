60e8a18477013f0e23f8ea0a324f1c2fb8cd41ef
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-10-26 17:53:29 -0300

    iniciando index front

--- server.js
+++ server.js
@@ -7,15 +7,23 @@ const http = require('http').createServer(app);
 
 const PORT = 3000;
 
-app.use(cors());
-
 const io = require('socket.io')(http, {
   cors: {
     origin: `http://localhost:${PORT}`, 
     methods: ['GET', 'POST'],
   } });
+  
+app.use(cors());
 
 app.set('view engine', 'ejs');
+
+io.on('connection', (socket) => {
+  console.log('Teste conexao');
+
+  socket.on('disconnect', () => {
+    console.log('saida');
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/chat/index.html'));
