bbf3419eaaf779343a21fb951ad90a2ff0e60a0b
Rafael <rmathscoach@gmail.com>
2021-10-26 14:57:46 -0300

    config server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// Faça seu código aqui
+require('dotenv').config();
+const app = require('express')();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
