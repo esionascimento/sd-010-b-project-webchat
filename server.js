3a5f023596723ba95f6781bb0b1fa4ca14f3b169
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-10-26 16:10:38 -0300

    feat: :construction: server.js basico e pastas controller e views :construction:

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-// Faça seu código aqui
-// instalação do dos pacotes básicos
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3000;
+
+app.use(cors());
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`ID: ${socket.id}`);
+});
+
+http.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}`);
+});
