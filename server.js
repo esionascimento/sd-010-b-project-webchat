7b79b47599d3f8e3da6233a46f466ccb8e638ed1
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-10-26 17:42:18 -0300

    Feat: Emit events

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
-  })
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log(`message: ${msg}`);
+  });
 });
 
 server.listen(3000, () => {
