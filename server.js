a56a510d70fc641dcdd2988c3816d917c4e6538b
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-10-26 15:01:21 -0300

    html/index

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http');
+
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/html/index.html`);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Escutando na porta: ${PORT}`);
+});
