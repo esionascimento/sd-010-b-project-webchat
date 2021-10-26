cfa0a285ec24577e1e640ea129be38614280cfa4
denis-rossati <denis.rossatiramos@gmail.com>
2021-10-26 16:45:25 -0300

    ftrocando o nome do db_url

--- src/view/js/script.js
+++ src/view/js/script.js
@@ -1,5 +1,13 @@
 const socket = window.io();
 
+const sendButton = document.querySelector('#sendButton');
+const messageInput = document.querySelector('#messageInput');
+const nicknameDisplay = document.querySelector('#userNickname');
+
+const isNewUser = () => {
+
+};
+
 const generateNickname = () => {
   let id = '';
   const alphabet = 'abcdefghijklmnopqrstuvwyxz';
@@ -11,14 +19,19 @@ const generateNickname = () => {
   return id;
 };
 
-const nickname = generateNickname();
-
-const sendButton = document.querySelector('#sendButton');
-const messageInput = document.querySelector('#messageInput');
+const olderOrNewerUser = () => {
+  const sessionNickname = sessionStorage
+};
 
 sendButton.addEventListener('click', () => {
   const chatMessage = messageInput.value;
   socket.emit('message', { chatMessage, nickname });
 });
 
-socket.on('message');
+socket.on('message', (message) => {
+
+});
+
+window.onload = () => {
+
+};
