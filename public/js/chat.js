a9ca84a05ed37debd0c4de3f32e8834341d320e5
Antenor <antenorzapata@gmail.com>
2021-10-26 09:21:09 -0300

    feat: add createHistory function

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,8 +10,9 @@ const ulUserList = document.querySelector('#user-list');
 // const getCurrentTime = require('../utils/getTime');
 const TESTID = 'data-testid';
 
-const randomNick = Math.random().toString(36).substring(2, 10)
-  + Math.random().toString(36).substring(2, 10);
+const randomNick =
+  Math.random().toString(36).substring(2, 10) +
+  Math.random().toString(36).substring(2, 10);
 
 let nickname = '';
 
@@ -37,8 +38,17 @@ const createMessage = (msg) => {
   ulList.appendChild(li);
 };
 
+const createHistory = (arrMsgs) => {
+  arrMsgs.forEach((msg) => {
+    const li = document.createElement('li');
+    li.setAttribute(TESTID, 'message');
+    li.textContent = `${msg.timestamp} - ${msg.nickname}: ${msg.message}`;
+    ulList.appendChild(li);
+  });
+};
+
 const createUser = (users, msgs = []) => {
-  // createHistory(msgs);
+  createHistory(msgs);
   ulUserList.innerHTML = '';
   users.forEach((user) => {
     const li = document.createElement('li');
@@ -52,5 +62,3 @@ socket.on('message', (msg) => createMessage(msg));
 socket.on('newUser', ({ users, msgs }) => createUser(users, msgs));
 socket.emit('newUser', randomNick);
 socket.emit('userConnected');
-// socket.on('userConnected', (msgs) => createHistory(msgs));
-// socket.on('disc', (curr, users) => removeUser(curr, users));
