bce1a2dd38d3bae2dc24d82397ef2293ef83bbf4
Felippe017 <felippecorrea2010@gmail.com>
2021-10-26 17:34:46 -0300

    Dev req01: motando servidor

--- server.js
+++ server.js
@@ -15,20 +15,30 @@ const io = require('socket.io')(http, {
     } });
   
 app.use(cors());
+app.use(express.json());
 
-/* const allMessage = []; */
+const allMessage = [];
 
 io.on('connection', (socket) => {
     console.log(`Usuário conectado. ID: ${socket.id} `);
-   /*  socket.on('message', ({ chatMessage, nickname }) => {
-        allMessage.push({ chatMessage, nickname });
-    }); */
+    socket.on('message', (data) => {
+      allMessage.push(data);
+      io.emit('message', data);
+    });
+
+    socket.emit('mensagem', 
+      console.log(allMessage, `dados enviados do servidor ${socket.id}`));
 });
 
-/* app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});  */
+app.get('/', async (req, res) => {
+  res.status(200).render('client');
+});
 
 http.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');
 });
+
+/* app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/client.ejs`);
+}); 
+ */
