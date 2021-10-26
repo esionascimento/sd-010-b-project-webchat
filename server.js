e5ca7811e78083b8c62011e5a6d01d9f0a7ef51a
Joao Vanelli <vanellijoao@gmail.com>
2021-10-26 14:36:02 -0300

    creating the server.js structure

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const httpServer = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'oi',
+  });
+});
+
+httpServer.listen(3001, () => console.log('listening on port 3000'));
