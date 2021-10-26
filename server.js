8350fb21acb03d93b3187f5b1559cd18dd213a21
Camila Arruda <camilaam1802@gmail.com>
2021-10-26 16:19:24 -0300

    iniciando peimeira estrutura

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 // Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser.json());
+
+const http = require('http').createServer(app);
+
+const { PORT } = process.env;
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: `http://localhost:${PORT}`,
+        methods: ['GET', 'POST'],
+    }
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(
+    cors({
+        origin: `http://localhost:${PORT}`,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Authorization'],
+    })
+);
+
+
+
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
