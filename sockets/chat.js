4a51c688daac023d3e07ea786b3b006128939806
Antenor <antenorzapata@gmail.com>
2021-10-26 09:11:53 -0300

    feat: create message event on serverr

--- sockets/chat.js
+++ sockets/chat.js
@@ -3,6 +3,12 @@ const getCurrentTime = require('../public/utils/getTime');
 const { removeUser, addUser, updateUser, getAllUsers } = require('../public/utils/usersHandlers');
 
 const conn = (io) => io.on('connection', (socket) => {
+    socket.on('message', ({ chatMessage, nickname }) => {
+      const messageObj = { message: chatMessage, nickname, timestamp: getCurrentTime() };
+      createMessage(messageObj);
+      io.emit('message', `${getCurrentTime()} ${nickname} ${chatMessage}`);
+    });
+
     socket.on('newUser', async (user) => {
       addUser(socket.id, user);
       const msgs = await getAll();
