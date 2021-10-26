b27ea6bfaf6ca5f677d75f3d2d2567df64cfc45c
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-10-26 16:27:18 -0300

    Feat: server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// Faça seu código aqui
+// setting up application like Doc: https://socket.io/get-started/chat#the-web-framework
+const express = require('express');
+
+const app = express();
+const http = require('http');
+
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
