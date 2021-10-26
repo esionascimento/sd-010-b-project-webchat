1ea9709738c92426e0567cc7a07a62fde085067b
Misael Bueno <misa.hbueno@gmail.com>
2021-10-26 16:50:08 -0300

    req1 test

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
 
-// começando o projeto!!!
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST'], 
+  } });
+
+  app.use(express.static(path.join(__dirname, '/public')));
+
+  require('./sockets/chat')(io);
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+
+  http.listen(3000, () => {
+    console.log('Servidor ouvindo na porta 3000');
+  });
