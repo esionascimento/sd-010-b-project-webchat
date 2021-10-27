ee54bd9cc57bb392f72bb8b62cadfb1dbc8caa63
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-27 19:39:36 -0300

    implementing the logic to attribute a random nickname for a new user, showing it in the screen and saving it in the session storage

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,6 +3,26 @@ const socket = window.io();
 const form = document.querySelector('form');
 const inputMessage = document.querySelector('#message-input');
 
+const generateNickName = () => {
+  const nickNameLenght = 16;
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'];
+  let newNickName = '';
+  for (let i = 1; i <= nickNameLenght; i += 1) {
+    const randomNumber = Math.floor((Math.random() * letters.length));
+    const randomLetter = letters[randomNumber];
+    newNickName += randomLetter;
+  }
+  return newNickName;
+};
+
+window.onload = function setNickname() {
+  const nickNameP = document.getElementById('nickname');
+  const newNickName = generateNickName();
+  sessionStorage.setItem('nickname', newNickName);
+  nickNameP.innerText = newNickName;
+};
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   socket.emit('message', { chatMessage: inputMessage.value, nickname: socket.id });
