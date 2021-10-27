6b69b1d1c761bb3811c0956b8a3346eb10edc5da
Camila Arruda <camilaam1802@gmail.com>
2021-10-27 15:38:41 -0300

    requisito 1 completo

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // Faça seu código aqui
 require('dotenv').config();
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
+// const path = require('path');
 
 app.use(bodyParser.json());
 
@@ -12,26 +13,51 @@ const http = require('http').createServer(app);
 
 const { PORT } = process.env;
 
+let message = '';
+const messagesArray = [];
+
+// Função de hora atual
+const horaCerta = () => {
+    const date = new Date();
+    const data = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    const hourAndMinute = `${date.getHours()}:${date.getMinutes()}`; 
+    return `${data} ${hourAndMinute}`;
+    }; 
+
 const io = require('socket.io')(http, {
     cors: {
         origin: `http://localhost:${PORT}`,
         methods: ['GET', 'POST'],
-    }
+    },
 });
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 
 app.use(
     cors({
         origin: `http://localhost:${PORT}`,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Authorization'],
-    })
+    }),
 );
 
+io.on('connection', (socket) => {
+    console.log(`Usuário conectado. ID: ${socket.id} `);
+    
+    socket.on('message', (data) => {
+        if (!data.nickname) {
+           return data.nickname === data.id;
+        }
+        message = ` ${horaCerta()} - ${data.nickname}: ${data.chatMessage}`;
+        console.log(message);
+        io.emit('message', message);
+    });
+});
 
-
+app.use('/', (req, res) => {
+        res.render('index', { message });
+});
 
 http.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');
