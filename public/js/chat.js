441a2c747759e4ab1b1af7a7b918da217c6d8cb5
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-10-27 16:33:58 -0300

    Requisito 2

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,18 +4,18 @@ const formMessage = document.querySelector('#formsMessage');
 const formNickName = document.querySelector('#formsNickName');
 const inputMessage = document.querySelector('#messageInput');
 const inputNickName = document.querySelector('#nickInput');
-inputNickName.innerText = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 16);
+const nick = document.querySelector('#nickName');
+// inputNickName.innerText = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 16);
 
 formMessage.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: inputMessage.value, nickname: inputNickName.innerText });
+  socket.emit('message', { chatMessage: inputMessage.value, nickname: nick.innerText });
   inputMessage.value = '';
   return false;
 });
 
 formNickName.addEventListener('submit', (e) => {
   e.preventDefault();
-  const nick = document.querySelector('#nickName');
   nick.innerText = inputNickName.value;
 });
 
