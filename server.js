39745fb098061cf097e7bfee1bedd034d5f9dd8a
Rafael <rmathscoach@gmail.com>
2021-10-27 08:55:56 -0300

    set server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+ });
+
+http.listen(3000, () => {
+ console.log('Server listening on port 3000');
+});
