e2e9ed7066a78224b7743cc7362bda9fbd8ff376
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-26 22:20:27 -0300

    configure database

--- models/connection.js
+++ models/connection.js
@@ -1,16 +1,19 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
+const { DB_URL } = process.env;
+const { DB_NAME } = process.env; 
+
 let schema = null;
 
 async function connection() {
   if (schema) return Promise.resolve(schema);
   return MongoClient
-    .connect(process.env.DB_URL, {
+    .connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((conn) => conn.db(DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
       return schema;
