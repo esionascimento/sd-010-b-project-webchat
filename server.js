454cf0b2b45a1fd9e8484d065c0b0cc746bf2cbc
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-10-27 16:46:50 -0300

    Req04 fished

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = 3000;
 
 const http = require('http').createServer(app);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.json());
 
 const io = require('socket.io')(http, {
@@ -18,8 +18,10 @@ const io = require('socket.io')(http, {
 });
 
 app.use(express.static(`${__dirname}/views`));
+let conectedUsers = [];
 
 io.on('connection', (socket) => {
+  io.emit('idConnect', socket.id);
   socket.on('SendMessage', (payload) => {
     const { chatMessage, nickName } = payload;
 
@@ -27,12 +29,23 @@ io.on('connection', (socket) => {
     const sendMensage = `$ ${dateAndHour} - ${nickName} -  ${chatMessage}`;
     io.emit('ReciveMessage', sendMensage);
   });
+
+  socket.on('UserOnline', (conectUser) => {
+    conectedUsers = [[...conectUser[0], socket.id], ...conectedUsers]
+      .filter((e) => e[0] !== conectUser[1]);
+    io.emit('allUsers', conectedUsers);
+  });
+
+  socket.on('disconnect', () => {
+    conectedUsers = conectedUsers.filter(([, id]) => id !== socket.id);
+    io.emit('allUsers', conectedUsers);
+  });
 });
 
 app.get('/', (req, res) => {
    res.render(path.join(__dirname, 'views', 'index.ejs'));
 });
 
-http.listen(3001, () => {
-  console.log('Servidor ouvindo na porta 3001');
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
 });
--- views/Script/index.js
+++ views/Script/index.js
@@ -1,14 +1,18 @@
 const socket = window.io();
 
 const randonNumbers = String(Math.random().toString(32).slice(2));
-const nickName = `User-${randonNumbers}`;
+let nickName = `User-${randonNumbers}`;
+let oldNick = '';
+let conectUsers = [];
+conectUsers.push(nickName);
 
 const inputMessage = document.getElementById('input-message');
 const sendButton = document.getElementById('button-message');
 const nickUser = document.getElementById('nick-name');
 const ul = document.getElementById('ul-message');
 const inputNick = document.getElementById('input-nick');
-const chageNickButton = document.getElementById('button-nick');
+const changeNickButton = document.getElementById('button-nick');
+const ulUserConect = document.getElementById('usersConect');
 nickUser.innerText = nickName;
 
 sendButton.addEventListener('click', (e) => {
@@ -16,16 +20,42 @@ sendButton.addEventListener('click', (e) => {
   const chatMessage = inputMessage.value;
   const payload = { nickName, chatMessage };
   socket.emit('SendMessage', payload);
- // inputMessage.value('');
+  inputMessage.value = '';
 });
 
+function userOnline() {
+  socket.emit('UserOnline', [conectUsers, oldNick]);
+}
 changeNickButton.addEventListener('click', (e) => {
-
+  e.preventDefault();
+  oldNick = nickName;
+  conectUsers = conectUsers.filter((element) => element !== nickName);
+  const newNick = inputNick.value;
+  conectUsers.unshift(newNick);
+  userOnline();
+  nickName = newNick;
+  nickUser.innerText = nickName;
+  inputNick.value = '';
 });
 
 socket.on('ReciveMessage', (msg) => {
-  console.log(msg);
   const li = document.createElement('li');
   li.innerHTML = msg;
   ul.appendChild(li);
 });
+
+socket.on('allUsers', (allUsers) => {
+  ulUserConect.innerText = '';
+  const newArray = allUsers.map((e) => e[0]);
+  const firstnick = newArray.filter((e) => e !== nickName);
+  [nickName, ...firstnick].filter((el) => el !== oldNick).forEach((element) => {
+    const li = document.createElement('li');
+    li.innerText = element;
+    ulUserConect.appendChild(li);
+    });
+  socket.emit('deleteUser', nickName);
+});
+
+window.onload = () => {
+  userOnline();
+};
