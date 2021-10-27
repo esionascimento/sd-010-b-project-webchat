91dc01fdf0b6459e7fe36a0c04f66b776dfebab9
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-27 20:41:54 -0300

    add connect and disconnect logs in socket

--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -1,7 +1,13 @@
 module.exports = (io) => io.on('connection', (socket) => {
   const dateNow = new Date().toLocaleString().replaceAll('/', '-');
-  
+
+  console.log('alguém se conectou');
+
   socket.on('message', ({ nickname, chatMessage }) => {
     io.emit('message', `${dateNow} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('alguém se desconectou');
   });
 });
