d88c7374b624e383ff366c5310e709f974d1f5d4
Daniel <darkdanielr@gmail.com>
2021-10-27 16:29:57 -0300

    req4 finalizado

--- server.js
+++ server.js
@@ -18,27 +18,57 @@ app.set('views', './view');
 
 // MOMENT
 
-io.on('connection', (socket) => {
-  let nick = '';
-  const onlineUsers = [];
+let newUsers = [];
 
-  console.log('alguem entrou');
-  console.log(onlineUsers);
-  socket.on('message', async (message) => { 
-    const { chatMessage, nickname } = message;
-    const date = new Date().toLocaleString().replace(/\//g, '-');
-    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+const mensagem = (message) => {
+  const { chatMessage, nickname } = message;
+  const date = new Date().toLocaleString().replace(/\//g, '-');
+  io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+};
+
+const edited = (nickEdited, socket) => {
+  const edit = newUsers.map((user) => {
+    if (user.id === socket.id) return { nick: nickEdited, id: socket.id };
+    return user;
+  });
+  newUsers = edit;
+  const onlyStrings = newUsers.map((users) => users.nick);
+  io.emit('onlineUsers', onlyStrings);
+};
+
+  const login = (n, socket) => {
+    const user = { nick: n, id: socket.id };
+    newUsers.push(user);
+    const onlyStrings = newUsers.map((users) => users.nick);
+    io.emit('onlineUsers', onlyStrings);
+  };
+
+io.on('connection', (socket) => {
+  // let nick = '';
+  console.log('fulano entrou');
+  socket.on('message', (message) => {
+    mensagem(message);
   });
 
-  socket.on('logedNick', (n) => { 
-    nick = n;
-    onlineUsers.push(nick);
-    io.emit('loggedUsers', onlineUsers);
+  socket.on('logedNick', (n) => {
+    login(n, socket);
+  });
+  
+  socket.on('nickEdtion', (nickEdited) => {
+    edited(nickEdited, socket);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('fulano vazou');
+    const diconected = newUsers.filter((users) => users.id !== socket.id);
+    newUsers = diconected;
+    const onlyStrings = newUsers.map((users) => users.nick);
+    io.emit('onlineUsers', onlyStrings);
   });
 });
 
 app.get('/', (_req, res) => {
-  res.status(200).render('index', { message: 'oi daniel' });
+  res.status(200).render('index');
 });
 
 http.listen(3000, () => console.log('Sever on 3000'));
