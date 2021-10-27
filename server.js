04d63c916e3ad1f75f04a5e95689b42591f1b91a
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-27 13:28:58 -0300

    refactor server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 
-const { PORT } = process.env;
+const { PORT } = process.env || '3000';
 
 const app = express();
 app.use(express.static(`${__dirname}/src`));
