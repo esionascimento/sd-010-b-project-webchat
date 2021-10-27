1ac21dab8e999c088eb2c90fa9ae60992a9418b8
arthur menezes <arthurmcorrea282@gmail.com>
2021-10-27 18:36:45 -0300

    desenvolvimento do chat para enviar mensagens de usuario a usuario, aparentemente, tudo funciona como deveria, mas os testes nao passam

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
 const express = require('express');
-
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const io = require('socket.io')(http, {
-    cors: {
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'POST'],
-    } });
-  
-  io.on('connection', (socket) => {
-    console.log(`Usuário conectado. ID: ${socket.id} `);
-    socket.emit('ola', socket.id);
-  });
-  app.use(express.static(__dirname + '/public'));
+const date = () => {
+  const data = new Date();
+  const dia = String(data.getDate()).padStart(2, '0');
+  const mes = String(data.getMonth() + 1).padStart(2, '0');
+  const ano = data.getFullYear();
+  return `${dia}-${mes}-${ano}`;
+};
 
-  require('./sockets/ping')(io);
-  require('./sockets/chat')(io);
+const hors = () => {
+  const data = new Date();
+  const hora = data.getHours()
+  const minutos = data.getMinutes()
+  const segundos = data.getSeconds()
+  return `${hora}:${minutos}:${segundos}`;
+};
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+io.on('connection', (socket) => {
+  socket.on('message', (mensageUser) => {
+    console.log(mensageUser.nickname);
+    const { chatMessage, nickname } = mensageUser;
+    const message = {
+      id: nickname,
+      message: chatMessage,
+      date: date(),
+      hora: hors(),
+    };
+    io.emit('message', message);
+  });
+});
 
-http.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
