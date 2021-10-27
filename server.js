5347fabf999c5fb0546303f9011a8c59dcd482c2
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-10-27 06:11:03 -0300

    Passos iniciais do Server feito

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(express.static(`${__dirname}/views`));
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado ${socket.id}`);
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
