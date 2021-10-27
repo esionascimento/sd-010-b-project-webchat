e980750de6d292f7a0d3b94501178e3a02863b7f
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-26 22:20:42 -0300

    create chat js

---
+++ src/js/chat.js
@@ -0,0 +1,38 @@
+const socket = window.io();
+
+const inputNickmame = document.querySelector('#nickname');
+const sendNickname = document.querySelector('#send_nickname');
+const inputMsg = document.querySelector('#messages__item');
+const sendMsg = document.querySelector('#send_message');
+
+function createMsg(msg) {
+  const msgUl = document.querySelector('.messages');
+  const li = document.createElement('li');
+  li.innerText = msg;
+
+  msgUl.appendChild(li);
+}
+
+const newUser = Math.random().toString(16).substr(2, 8) + Math.random().toString(16).substr(2, 8);
+
+window.onload = () => {
+  createMsg(newUser);
+};
+
+sendNickname.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  createMsg(inputNickmame.value);
+  inputNickmame.value = '';
+});
+
+sendMsg.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  socket.emit('message', { nickname: newUser, chatMessage: inputMsg.value });
+
+  inputMsg.value = '';
+  return false;
+});
+
+socket.on('message', (msg) => createMsg(msg));
