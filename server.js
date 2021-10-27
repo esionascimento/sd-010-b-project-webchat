08f795bff744dd3f35730056c81bd1a9a0640d01
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-10-27 18:07:26 -0300

    chat semi-configurado, mensagens sao trocadas entre os clientes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -5,7 +6,8 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-const socketIoServer = require('http').createServer();
+const app = express();
+const socketIoServer = require('http').createServer(app);
 
 const io = require('socket.io')(socketIoServer, {
   cors: {
@@ -13,8 +15,6 @@ const io = require('socket.io')(socketIoServer, {
     methods: ['GET', 'POST'],
   },
 });
-
-const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -30,13 +30,34 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// io.on('connection', (socket) => {
-//   socket.on('posting', (data) => {
-//     console.log(data);
-//     io.emit('notification', { title: 'NAO_MODERADO', message: data.message });
-//   });
+io.on('connection', (socket) => {
+  console.log(`${socket.id} conectado`);
+  socket.on('message', (data) => {
+    const { chatMessage, nickname } = data;
+    io.emit('message', { chatMessage, nickname });
+  });
+});
+
+// app.listen(PORT, () => {
+//   console.log(`App listening onx port ${PORT}`);
 // });
 
-app.listen(PORT, () => {
-  console.log(`App listening onx port ${PORT}`);
+const dataDb = { messages: ['09-10-2020 2:35:09 PM - Joel: Olá meu caros amigos!',
+'09-10-2024 2:35:09 PM - Paulin: CATAPIMBAS! CADÊ MEU CAFÉ?'],
+users: ['Joel', 'Paulin', 'DEUS'] };
+app.get('/', (req, res) => res.status(200).render('chat', { dataDb }));
+
+app.post('/', (req, res) => {
+  const { message } = req.body;
+  if (!message) {
+    return res.status(422).json({ message: 'Missing title or message' });
+  }
+  // dataDb.messages.push(message);
+  console.log(message);
+  // io.emit('message', { message });
+  return res.status(200).json({ message: 'Message sent' });
 });
+
+socketIoServer.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+ });
