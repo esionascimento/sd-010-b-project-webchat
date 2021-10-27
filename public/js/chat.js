01bd1ef6f4e15c906f3a48dea1f7011e3b286d40
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-27 01:41:33 -0300

    function to add message and rename user

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,42 @@
 const socket = window.io('http://localhost:3000');
+const ID = '#user-name';
 
 const formMessage = document.querySelector('.form-message');
 formMessage.addEventListener('submit', (event) => {
   event.preventDefault();
-  const newMessage = event.target.querySelector('.new-message').value;
+  const { target } = event;
+  const newMessage = target.querySelector('#message-box').value;
+  const userName = document.querySelector(ID).innerText;
+  
+  socket.emit('message', { chatMessage: newMessage, nickname: userName });
 
-  socket.emit('message', { chatMessage: newMessage, nickname: '' });
+  target.target.querySelector('#message-box').value = '';
 });
 
+const formUser = document.querySelector('.form-user');
+formUser.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const { target } = event;
+  const newNick = target.querySelector('#nickname-box').value;
+  console.log(newNick);
+
+  localStorage.setItem('userSeted', newNick);
+  document.querySelector(ID).innerHTML = newNick;
+  target.querySelector('#nickname-box').value = '';
+});
+
+// sockets events
 socket.on('message', (message) => {
-  console.log(message);
+  const containerMessages = document.querySelector('.container-messages');
+  const p = document.createElement('p');
+  p.setAttribute('data-testid', 'message');
+  p.innerText = message;
+
+  containerMessages.appendChild(p);
+});
+
+socket.on('connected', (user) => {
+  // colocar condição que testa se usuário existe no localstorge
+
+  document.querySelector(ID).innerHTML = user;
 });
