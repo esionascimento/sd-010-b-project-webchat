a36a37a69761c5c68d56f8e5ac035497289fc5a9
Leandro Reis <lleandrofr@gmail.com>
2021-10-27 16:28:22 -0300

    feat: save messagens on db

---
+++ src/models/messagesModel.js
@@ -0,0 +1,17 @@
+const connection = require('./connection');
+
+const addMessage = async (message, nickname, timestamp) => {
+  const db = await connection();
+
+  await db.collection('messages').insertOne({ message, nickname, timestamp });
+};
+
+const getAllMessages = async () => {
+  const db = await connection();
+  return db.collection('messages').find().toArray();
+};
+
+module.exports = {
+  addMessage,
+  getAllMessages,
+};
--- src/sockets/message.js
+++ src/sockets/message.js
@@ -1,9 +1,12 @@
+const messagesModel = require('../models/messagesModel');
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('message', (message) => {
+    socket.on('message', async (message) => {
       const { chatMessage, nickname } = message;
-      const now = new Date().toLocaleString().replace(/\//g, '-');
-      // thanks for the tip, Lucas Martins. https://github.com/tryber/sd-010-b-project-webchat/pull/14
+      const now = new Date().toLocaleString().replace(/\//g, '-'); // thanks for the tip, Lucas Martins. https://github.com/tryber/sd-010-b-project-webchat/pull/14
+      
+      await messagesModel.addMessage(chatMessage, nickname, now);
       io.emit('message', `${now} - ${nickname}: ${chatMessage}`);
     });
   });
