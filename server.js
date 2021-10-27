f5d5be6e6d8b393ec7611c22f9a6c28058f2bbee
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-27 15:14:01 -0300

    starting initial configuration for the webchat server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const htttp = require('http').createServer(app);
+
+const io = require('socket.io')(htttp, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+require('./sockets/chat')(io);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+htttp.listen(3000, () => {
+  console.log('Listening port 3000');
+});
