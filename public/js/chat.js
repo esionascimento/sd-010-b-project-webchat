58ab6c34d2442ab5eb86d1ac3ee5305aff8e7628
isTorres <ismaeltorres612@gmail.com>
2021-10-27 19:47:42 -0300

    refactored and organize the functions

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,76 @@
 const socket = window.io();
 
-socket.on('userConnect', (id) => {
-  const usersList = document.querySelector('#usersList');
-  const userId = document.createElement('li');
-  userId.innerText = `User ID: ${id}`;
-  usersList.appendChild(userId);
+const randomNickname = () => {
+  let randomString = '';
+  do { randomString += Math.random().toString(36).substr(2); } while (randomString.length < 16);
+  randomString = randomString.substr(0, 16);
+  
+  return randomString;
+};
+// https://gist.github.com/6174/6062387
+
+// Choose username:
+const nicknameImput = document.getElementById('username');
+const nickChangeBtn = document.getElementById('usernameBtn');
+const userNickname = document.getElementById('userNickname');
+const usersList = document.getElementById('usersList');
+
+// const params = (new URL(document.location)).searchParams;
+
+let nickname = randomNickname();
+
+nickChangeBtn.addEventListener('click', () => {
+  const oldNick = nickname;
+  const newNick = nicknameImput.value;
+
+  nickname = nicknameImput.value;
+  userNickname.innerText = nickname;
+
+  socket.emit('chageNicknames', { oldNick, newNick });
+
+  nicknameImput.value = '';
 });
 
-const form = document.querySelector('form');
-const inputMessage = document.querySelector('#messageInput');
+const createNick = (nick, newNick) => {
+  const li = document.createElement('li');
+  li.innerText = nick;
+  if (newNick === nickname) li.setAttribute('data-testid', 'online-user');
+  usersList.appendChild(li);
+};
 
-form.addEventListener('click', (e) => {
+const createArrayOfNicks = (array) => {
+  usersList.innerHTML = '';
+  array.forEach((nick) => createNick(nick, nickname));
+};
+
+// socket.on('userConnect', (name) => {
+//   if (!name) return false;
+
+//   nicknameUser.innerText = `User connected: ${name}`;
+//   usersList.appendChild(nicknameUser);
+// });
+
+// Chat messages:
+const sendBtn = document.getElementById('sendBtn');
+const inputMessage = document.getElementById('messageInput');
+
+sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  socket.emit('clientMessage', inputMessage.value);
+  // const nick = params.get('username');
+
+  // if (!nick) {
+  //   return socket.emit('clientMessage', {
+  //       nickname,
+  //       message: inputMessage.value,
+  //   }); 
+  // }
+  
+  socket.emit('clientMessage', {
+    nickname,
+    message: inputMessage.value,
+  });
+
   inputMessage.value = '';
-  return false;
 });
 
 const createMessage = (message) => {
@@ -24,4 +80,16 @@ const createMessage = (message) => {
   messagesUl.appendChild(li);
 };
 
-socket.on('serverMessage', (message) => createMessage(message));
+socket.on('message', (recivedMessage) => createMessage(recivedMessage));
+
+socket.on('updateNicknames', (nicknames) => {
+  const nicknamesList = nicknames.filter((el) => el !== nickname);
+  nicknamesList.unshift(nickname);
+  createArrayOfNicks(nicknamesList);
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+});
+
+window.onload = () => {
+  userNickname.innerHTML = nickname;
+  socket.emit('userLogin', nickname);
+};
