462cab468a6edab7c641d8af3fd68b5de9f4439f
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-27 19:21:44 -0400

    req 01

---
+++ controllers/chatController.js
@@ -0,0 +1,5 @@
+const getChat = (req, res) => res.status(200).render('chat');
+
+module.exports = {
+  getChat,
+}; 
--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-// Faça seu código aqui
+// Faça seu código aqui// Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, { 
+  cors: { 
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST'],
+  },
+});
+
+const chatController = require('./controllers/chatController');
+const chat = require('./sockets/chat');
+
+chat(io);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', chatController.getChat);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}!`)); 
