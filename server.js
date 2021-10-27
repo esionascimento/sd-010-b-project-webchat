8884f368f627dd0ad88ce73ec2b7c4979558c00e
Lara-Capila <laracapilak@gmail.com>
2021-10-27 19:06:06 -0300

    Requisito 1 - feito

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3000;
+
+let message = [];
+
+const formattedDate = () => {
+  const date = new Date();
+  const currentDate = `${date.getDay()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+  const fullHour = `${date.getHours()}:${date.getMinutes()}`;
+  const pmOrAm = fullHour < 12 ? 'AM' : 'PM';
+  return `${currentDate} ${fullHour} ${pmOrAm}`;
+};
+
+app.use(cors());
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id}`);
+
+  socket.on('message', (data) => {
+    console.log(data);
+    message = `${formattedDate()} - ${data.nickname}: ${data.chatMessage}`;
+    io.emit('message', message);
+  });
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', (_req, res) => {
+  res.render('webchat', { message });
+});
+
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
