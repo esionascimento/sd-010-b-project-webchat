6da360686795f8ecda75be6c558fb40ef721ec74
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-10-27 15:27:45 -0300

    done: requisito 3

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 const PORT = process.env.PORT || 3000;
+const userList = [];
 
 const io = require('socket.io')(http, {
   cors: {
@@ -22,10 +23,26 @@ app.get('/', (req, res) => {
 app.use(express.static(`${__dirname}/views`));
 
 io.on('connection', (socket) => { // agradecimentos Lucas Martins da Silva PR: https://github.com/tryber/sd-010-b-project-webchat/pull/14
-  socket.on('message', (message) => {
-    const { chatMessage, nickname } = message;
+  socket.on('message', ({ chatMessage, nickname }) => {
     const dateNow = new Date().toLocaleString().replace(/\//g, '-');
     io.emit('message', `${dateNow} - ${nickname} ${chatMessage}`);
+  });
+
+  socket.on('userConnected', (nickname) => {
+    userList.push({ nickname, id: socket.id });
+    io.emit('updateUserList', userList);
+  });
+
+  socket.on('nickUpdate', ({ nickname, oldNick }) => {
+    const oldNickIndex = userList.findIndex((e) => e.nickname === oldNick);
+    userList.splice(oldNickIndex, 1, { nickname, id: socket.id });
+    io.emit('updateUserList', userList);
+  });
+
+  socket.on('disconnect', ({ id }) => {
+    const index = userList.findIndex((user) => user.id === id);
+    userList.splice(index, 1); 
+    io.emit('updateUserList', userList);
   });
 });
 
--- views/js/script.js
+++ views/js/script.js
@@ -3,13 +3,22 @@ const socket = window.io();
   let nickname = Math.random()
   .toString(16).substr(2, 8) + Math.random().toString(16).substr(2, 8); // agradecimentos Ederson Rodrigues PR: https://github.com/tryber/sd-010-b-project-webchat/pull/16
 
+  // let arrayOnlineUsers = [];
+
     const btnEnviarMsg = document.getElementById('enviar-msg');
     const ulMsg = document.getElementById('ul-msg');
-    const nickTela = document.getElementById('nick-tela');
-    nickTela.innerText = nickname;
+    // const nickTela = document.getElementById('nick-tela');
+    // nickTela.innerText = nickname;
     const chatInput = document.getElementById('input-message');
     const newNick = document.getElementById('input-nick');
     const btnTrocarNick = document.getElementById('enviar-novo-nick');
+    const ulOnlineUser = document.querySelector('#ul-online-user');
+
+    socket.emit('userConnected', nickname);
+
+    // socket.on('userConnected', (userList) => {
+    //   arrayOnlineUsers = userList;
+    // });
 
     btnEnviarMsg.addEventListener('click', (event) => {
       event.preventDefault();
@@ -20,9 +29,12 @@ const socket = window.io();
 
     btnTrocarNick.addEventListener('click', (event) => {
       event.preventDefault();
+      const oldNick = nickname;
       nickname = newNick.value;
-      nickTela.innerText = nickname;
+      // nickTela.innerText = nickname;
       newNick.value = '';
+      socket.emit('nickUpdate', { nickname, oldNick });
+      // userConnected();
     });
 
     socket.on('message', (string) => {
@@ -31,3 +43,33 @@ const socket = window.io();
       liMsg.innerText = string;
       ulMsg.appendChild(liMsg);
     });
+
+    socket.on('updateUserList', (array) => {
+      ulOnlineUser.innerHTML = '';
+      array.forEach((user) => {
+        const liUser = document.createElement('li');
+        liUser.setAttribute('data-testid', 'online-user');
+        liUser.innerText = user.nickname;
+        if (socket.id === user.id) {
+          ulOnlineUser.prepend(liUser); // dica do Leandro Reis
+        } else {
+        ulOnlineUser.appendChild(liUser);
+        }
+    });
+      }); 
+
+    // socket.on('nickSwap', (array) => {
+    //   ulOnlineUser.innerHTML = '';
+    //   array.forEach((user) => {
+    //     const liUser = document.createElement('li');
+    //     liUser.setAttribute('data-testid', 'online-user');
+    //     liUser.innerText = user;
+    //     ulOnlineUser.appendChild(liUser);
+    // });
+    //   });
+      
+    // socket.on('newArrayNick', (array) => {});
+
+    // window.onload = () => {
+    //   userConnected();
+    // };
