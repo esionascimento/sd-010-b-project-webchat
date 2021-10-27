ebedf5b55a0bb8dd63a69e88b27dc9495ab4dd62
Huggo Parcelly <h.parcelly@gmail.com>
2021-10-27 18:02:51 -0300

    informa os clientes que estao online

--- sockets/users.js
+++ sockets/users.js
@@ -1,8 +1,10 @@
-const nicks = [];
+let nicks = [];
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    let userName;
     socket.on('nickname', (nick) => {
+      userName = nick;
       nicks.push(nick);
       io.emit('nickname', nicks);
     });
@@ -10,6 +12,12 @@ module.exports = (io) => {
     socket.on('newNickname', ({ newNick, nickname }) => {
       const findIndexUser = nicks.findIndex((user) => user === nickname);
       nicks[findIndexUser] = newNick;
+      userName = newNick;
+      io.emit('nickname', nicks);
+    });
+
+    socket.on('disconnect', () => {
+      nicks = nicks.filter((user) => user !== userName);
       io.emit('nickname', nicks);
     });
   });
