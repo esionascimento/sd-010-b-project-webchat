e599d7eb8d3a0d3f3405e214d26087ec94fe81eb
legalvao1 <legalvao1@gmail.com>
2021-10-27 12:42:46 -0300

    Mudando a formataçao da mensagem para o back-end e substituindo as / das datas para -

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,8 +1,12 @@
 module.exports = (io) => io.on('connection', (socket) => {
   console.log(`${socket.id} se conectou`);
 
-  socket.on('message', (message) => {
-    io.emit('message', message);
+  socket.on('message', ({ chatMessage, nickname }) => {
+    /* SOURCE https://stackoverflow.com/questions/42862729/convert-date-object-in-dd-mm-yyyy-hhmmss-format
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString */
+    const date = new Date();
+    const msg = `${date.toLocaleString('en-US')} - ${nickname}: ${chatMessage}`.replace(/\//g, '-');
+    io.emit('message', msg);
   });
 
   socket.on('disconnect', () => {
