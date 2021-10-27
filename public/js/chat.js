2dea6f637c3fe0d113f2c43c8c141983c7f72b35
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-27 17:26:23 -0300

    adds the logic so the front-end may show the message

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,18 @@ const inputMessage = document.querySelector('#message-input');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('message', { message: inputMessage.value, nickname: 'padrão' });
+  socket.emit('message', { chatMessage: inputMessage.value, nickname: socket.id });
   inputMessage.value = '';
   return false;
 });
+
+const createMessage = (message) => {
+  const messagesUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+  console.log(message);
+  const { nickname, chatMessage, time } = message;
+  li.innerText = `${nickname}: ${chatMessage} - ${time}`;
+  messagesUl.appendChild(li);
+};
+
+socket.on('serverMessage', (message) => createMessage(message));
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,11 @@
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', (data) => {
     console.log(`Mensagem de ${data.nickname}: ${data.message}`);
+    const time = new Date();
+    const today = `${time.getDate()}-${time.getMonth()}-${time.getFullYear()}`;
+    const moment = `${time.getHours()}:${time.getMinutes()}`;
+    const emission = { ...data, time: `${today} at ${moment}` };
+    console.log(emission);
+    io.emit('serverMessage', emission);
   });
 });
