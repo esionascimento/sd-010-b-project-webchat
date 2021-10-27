b9810d323b258a423f14cfef640e63928319b069
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-10-26 23:58:23 -0300

    primeiras configuracoes do server.js

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-// Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const socketIoServer = require('http').createServer();
+
+const io = require('socket.io')(socketIoServer, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(
+  cors({
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// io.on('connection', (socket) => {
+//   socket.on('posting', (data) => {
+//     console.log(data);
+//     io.emit('notification', { title: 'NAO_MODERADO', message: data.message });
+//   });
+// });
+
+app.listen(PORT, () => {
+  console.log(`App listening onx port ${PORT}`);
+});
