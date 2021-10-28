78ec963fadc88a981c18bbabd782ee48c2e2bd64
legalvao1 <legalvao1@gmail.com>
2021-10-28 20:32:23 -0300

    Criando uma funçao que cria li e removendo codigo repetido

--- sockets/chat.js
+++ sockets/chat.js
@@ -14,22 +14,22 @@ const users = [];
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString */
 
 module.exports = (io) => io.on('connection', (socket) => {
-  let randomNickname = generateNickname();
-  socket.emit('saveNickname', randomNickname);
-  io.emit('generateNickname', randomNickname);
+  let userNickname = generateNickname();
+  socket.emit('saveNickname', userNickname);
+  io.emit('onlineUsers', userNickname);
   if (users.length > 0) users.forEach((user) => socket.emit('onlineUsers', user));
-  users.push(randomNickname);
+  users.push(userNickname);
   socket.on('message', ({ chatMessage, nickname }) => {
     io.emit('message', 
     `${new Date().toLocaleString('en-US')} - ${nickname}: ${chatMessage}`.replace(/\//g, '-'));
   });
   socket.on('updateNickname', ({ previousNickname, atual }) => {
     users.splice(previousNickname, 1, atual);
-    randomNickname = atual;
+    userNickname = atual;
     io.emit('updateNickname', { previousNickname, atual });
   });
   socket.on('disconnect', () => {
-    users.splice(randomNickname, 1);
-    io.emit('offLineUser', randomNickname);
+    users.splice(userNickname, 1);
+    io.emit('offLineUser', userNickname);
   });
 });
