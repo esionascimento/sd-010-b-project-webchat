60d526927e5f2c425f2fc5c9a72930e542b2459e
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-10-28 17:10:27 -0300

    setting user

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const express = require('express');
 const path = require('path');
 const { createServer } = require('http');
@@ -11,6 +12,17 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const generateRandomString = (num) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result1 = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < num; i += 1) {
+    result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result1;
+};
+
 const io = require('socket.io')(httpServer, {
   cors: {
     origin: `http://localhost:${PORT}`,
@@ -18,10 +30,23 @@ const io = require('socket.io')(httpServer, {
   },
 });
 
-require('./sockets/chat.js')(io);
-
 app.get('/', (req, res) => {
   res.render('webChat');
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+
+  socket.on('message', (message) => {
+    io.emit(
+      'message',
+      `${moment(new Date()).format('DD-MM-yyyy HH:mm:ss')} - ${
+        message.nickname
+      }: ${message.chatMessage}`,
+    );
+  });
+
+  socket.emit('setUser', generateRandomString(16));
 });
 
 httpServer.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
--- sockets/chat.js
+++ sockets/chat.js
@@ -3,20 +3,12 @@ const moment = require('moment');
 module.exports = (io) =>
   io.on('connection', (socket) => {
     console.log(socket.id);
-
     socket.on('message', (message) => {
       io.emit(
         'message',
         `${moment(new Date()).format('DD-MM-yyyy HH:mm:ss')} - ${
           message.nickname
         }: ${message.chatMessage}`,
-      );
-    });
-
-    socket.on('disconnect', () => {
-      socket.broadcast.emit(
-        'serverMessage',
-        `Xiii! ${socket.id} acabou de se desconectar! :(`,
       );
     });
   });
