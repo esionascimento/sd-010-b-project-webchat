3ccf5f5f89684ff34578fcfcbcbb434a31b68a7f
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-10-28 16:27:29 -0300

    emit/on correction

--- sockets/chat.js
+++ sockets/chat.js
@@ -4,9 +4,9 @@ module.exports = (io) =>
   io.on('connection', (socket) => {
     console.log(socket.id);
 
-    socket.on('clientMessage', (message) => {
+    socket.on('message', (message) => {
       io.emit(
-        'serverMessage',
+        'message',
         `${moment(new Date()).format('DD-MM-yyyy HH:mm:ss')} - ${
           message.nickname
         }: ${message.chatMessage}`,
