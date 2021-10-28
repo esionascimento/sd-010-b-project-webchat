edfb790774cf46e9966d98c57c90f25fe4321249
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-10-28 13:05:41 -0300

    Requisito 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,20 +14,71 @@ const formMessage = document.querySelector('#formsMessage');
 const formNickName = document.querySelector('#formsNickName');
 const inputMessage = document.querySelector('#messageInput');
 const inputNickName = document.querySelector('#nickInput');
-const nick = document.querySelector('#nickName');
+// const nick = document.querySelector('#nickName');
 
-window.onload = () => { nick.innerText = makeid(16); };
+const createUserOn = (nickname) => {
+  const userUl = document.querySelector('#userson');
+  const li = document.createElement('li');
+  li.innerText = nickname;
+  li.id = 'nickname';
+  li.setAttribute('data-testid', 'online-user');
+  userUl.appendChild(li);
+};
+
+const updateUserOn = (changeUser) => {
+  // console.log(changeUser);
+  const list = document.querySelectorAll('#nickname');
+  // console.log(list);
+  list.forEach((element) => {
+    console.log(element.innerText); 
+    const text = element.innerText;
+    if (text.includes(changeUser.elementName)) {
+      // console.log('aqui');
+      const li = element;
+      li.innerText = changeUser.nickname;
+    }
+  });
+};
+
+const updateUserOff = (name) => {
+  // console.log(changeUser);
+  const listUl = document.querySelector('#messages');
+  const list = document.querySelectorAll('#nickname');
+  console.log(list, name);
+  list.forEach((element) => {
+    console.log(element.innerText); 
+    const text = element.innerText;
+    if (text.includes(name)) {
+      // console.log('aqui');
+     listUl.removeChild(element);
+    }
+  });
+  socket.disconnect();
+};
+
+window.onload = () => {
+  socket.emit('userOn', makeid(16)); 
+};
+
+function saveFormData() {
+    console.log('saved');
+}
+window.beforeunload = () => {
+  saveFormData();
+    return null;
+};
 
 formMessage.addEventListener('submit', async (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: inputMessage.value, nickname: nick.innerText });
+  const li = document.querySelectorAll('#nickname');
+  socket.emit('message', { chatMessage: inputMessage.value, nickname: li[0].innerText });
   await fetch('http://localhost:3000', {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ message: inputMessage.value, nickname: nick.innerText }),
+    body: JSON.stringify({ message: inputMessage.value, nickname: li[0].innerText }),
   });
   inputMessage.value = '';
   return false;
@@ -35,15 +86,21 @@ formMessage.addEventListener('submit', async (e) => {
 
 formNickName.addEventListener('submit', (e) => {
   e.preventDefault();
-  nick.innerText = inputNickName.value;
+  const li = document.querySelectorAll('#nickname');
+  socket.emit('updateUserOn', { nickname: inputNickName.value, elementName: li[0].innerText });
 });
 
 const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
+  console.log(messagesUl);
   const li = document.createElement('li');
+  console.log(li);
   li.innerText = message;
   li.setAttribute('data-testid', 'message');
   messagesUl.appendChild(li);
 };
 
 socket.on('message', (message) => createMessage(message));
+socket.on('userOn', (nickname) => createUserOn(nickname));
+socket.on('updateUserOn', (changeUser) => updateUserOn(changeUser));
+socket.on('userOff', (name) => updateUserOff(name));
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,6 +1,21 @@
 const { dateNow } = require('../helper/date');
  
  module.exports = (io) => io.on('connection', (socket) => {
+  socket.on('userOn', (nickname) => {
+    console.log(`ONline ${nickname}`);
+    io.emit('userOn', nickname);
+  });
+
+  socket.on('disconnect', (name) => {
+    console.log('desconecata', name);
+    socket.broadcast.emit('userOff', name);
+  });
+
+  socket.on('updateUserOn', (changeUser) => {
+    console.log(`Update ${changeUser}`);
+    io.emit('updateUserOn', changeUser);
+  });
+
   socket.on('message', (message) => {
     console.log(`Mensagem ${message.nickname}`);
     io.emit('message', `${dateNow()} - ${message.nickname}: ${message.chatMessage}`);
