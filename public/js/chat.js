b03665038e522550d5ecad210ffcecd47f9d4517
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-10-28 19:49:24 -0300

    Requisito 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -83,14 +83,6 @@ formMessage.addEventListener('submit', async (e) => {
   e.preventDefault();
   const li = document.querySelectorAll('#nickname');
   socket.emit('message', { chatMessage: inputMessage.value, nickname: li[0].innerText });
-  await fetch('http://localhost:3000', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ message: inputMessage.value, nickname: li[0].innerText }),
-  });
   inputMessage.value = '';
   return false;
 });
