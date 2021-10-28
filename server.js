9e42b332c23f49851cf6120822c8b117cb86383f
Felippe017 <felippecorrea2010@gmail.com>
2021-10-28 12:23:18 -0300

    REFACT: tirando consoles.log e comentarios desnecessarios

--- server.js
+++ server.js
@@ -21,19 +21,6 @@ app.use(express.json());
 
 const allMessage = [];
 console.log(allMessage);
-/* const dataAtual = new Date();
-const dia = (dataAtual.getDate() < 10 ? '0' : '') + dataAtual.getDate();
-const mes = ((dataAtual.getMonth() + 1) < 10 ? '0' : '') + (dataAtual.getMonth() + 1);
-
-// Peguei no site https://www.devmedia.com.br/como-criar-um-chat-com-node-js/33719
-function dataHora() {
-  const ano = dataAtual.getFullYear();
-  const hora = (dataAtual.getHours() < 10 ? '0' : '') + dataAtual.getHours();
-  const minuto = (dataAtual.getMinutes() < 10 ? '0' : '') + dataAtual.getMinutes();
-  const segundo = (dataAtual.getSeconds() < 10 ? '0' : '') + dataAtual.getSeconds();
-  const dataFormatada = `${dia}-${mes}-${ano} ${hora}:${minuto}:${segundo}`;
-  return dataFormatada;
-} */
 
 const newDataHora = require('./utils/getDate');
 const chat = require('./utils/format');
