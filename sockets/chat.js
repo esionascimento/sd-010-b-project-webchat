b0190d76512fe18e762d976ad8a4e7c03d6b65ca
legalvao1 <legalvao1@gmail.com>
2021-10-28 10:18:47 -0300

    Alterando o nome de um usuario, salvando e exibindo no chat de todos

--- sockets/chat.js
+++ sockets/chat.js
@@ -10,23 +10,26 @@ const generateNickname = () => {
 
 const users = [];
 
+/* SOURCE https://stackoverflow.com/questions/42862729/convert-date-object-in-dd-mm-yyyy-hhmmss-format
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString */
+
 module.exports = (io) => io.on('connection', (socket) => {
-   const randomNickname = generateNickname();
+  let randomNickname = generateNickname();
+  socket.emit('saveNickname', randomNickname);
   io.emit('generateNickname', randomNickname);
   if (users.length > 0) users.forEach((user) => socket.emit('onlineUsers', user));
   users.push(randomNickname);
-
   socket.on('message', ({ chatMessage, nickname }) => {
-    /* SOURCE https://stackoverflow.com/questions/42862729/convert-date-object-in-dd-mm-yyyy-hhmmss-format
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString */
-    const date = new Date();
-    const msg = `${date.toLocaleString('en-US')} - ${nickname}: ${chatMessage}`.replace(/\//g, '-');
-    io.emit('message', msg);
+    io.emit('message', 
+    `${new Date().toLocaleString('en-US')} - ${nickname}: ${chatMessage}`.replace(/\//g, '-'));
   });
-
+  socket.on('updateNickname', ({ previousNickname, atual }) => {
+    users.splice(previousNickname, 1, atual);
+    randomNickname = atual;
+    io.emit('updateNickname', { previousNickname, atual });
+  });
   socket.on('disconnect', () => {
     users.splice(randomNickname, 1);
     io.emit('offLineUser', randomNickname);
-    console.log(`${randomNickname} saiu`);
   });
 });
