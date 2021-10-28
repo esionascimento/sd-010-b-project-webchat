6f55478e8be24b21881f6eaf0a2c8f3460ea09eb
Thiago Leite <leite.tjs@gmail.com>
2021-10-28 18:38:05 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (_req, res) => {
+  res.send('<h1>Welcome!</h1>');
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
