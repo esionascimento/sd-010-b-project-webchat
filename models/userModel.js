3e0e2ce6be21909b03cd8200a1d51a12022c0eee
Misael Bueno <misa.hbueno@gmail.com>
2021-10-28 15:49:26 -0300

    req4 feito

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,24 @@
 const listUsers = [];
 
 const newUser = (socketId, userName) => {
+  if (listUsers.length > 0) {
+    const userExist = listUsers.findIndex((user) => user.id === socketId);
+
+    if (userExist !== -1) {
+      listUsers[userExist].name = userName;
+      return listUsers;
+    }
+
+    const objUser = { id: socketId, name: userName };
+    listUsers.push(objUser);
+    // console.log(listUsers.length);
+    // console.log(listUsers);
+    return listUsers;
+  }
+
   const objUser = { id: socketId, name: userName };
-  listUsers.push(objUser);
-  console.log(listUsers.length);
-  console.log(listUsers);
-  return listUsers;
+    listUsers.push(objUser);
+    return listUsers;
 };
 
 const removeUser = (socketId) => {
