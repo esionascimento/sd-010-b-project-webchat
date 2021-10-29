8d22c2fa977cebb546a9893f89c9c084c019e034
Jessie <miss_sc_jessi@hotmail.com>
2021-10-29 18:21:25 -0300

    req 4

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,7 +2,7 @@ const messageModel = require('../models/messageModel');
 
 const listMessages = async (req, res) => {
   const messages = await messageModel.getAll();
-  return res.render('board', { messages });
+  res.render('board', { messages });
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -21,16 +21,27 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', messageController.listMessages);
+let usersOn = [];
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
+  // console.log(socket.id);
   socket.on('message', async (post) => {
     const { nickname, chatMessage } = post;
     const data = moment().format('DD-MM-yyyy HH:mm:ss A');
-    const message = `${data} - ${nickname} ${chatMessage}`;
-
+    const message = `${data} - ${nickname}: ${chatMessage}`;
     await messageModel.insertMessage(chatMessage, nickname, data);
-
     io.emit('message', message);
+  });
+  
+  socket.on('userOn', (user) => {
+    usersOn = usersOn.filter((id) => socket.id !== id.id);
+    usersOn.push({ id: socket.id, user });
+    io.emit('usersOn', usersOn);
+  });
+
+  socket.on('disconnect', () => {
+    usersOn = usersOn.filter((id) => socket.id !== id.id);
+    io.emit('usersOn', usersOn);
   });
 });
 
