121176fc22aa9d7893ad5df6d5643423823eead8
giovani.diccini <giovani.diccini@gmail.com>
2021-10-29 08:13:11 -0300

    initial commit - implements server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, _res) => console.log('ok'));
+
+http.listen(PORT, () => {
+  console.log(`Listen on ${PORT}`);
+});
