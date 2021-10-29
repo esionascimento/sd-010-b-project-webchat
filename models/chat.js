f3cdeb874d874aa88d89afa95b02b14329d2e73f
isTorres <ismaeltorres612@gmail.com>
2021-10-28 22:16:16 -0300

    add function to save msgs on db

--- models/chat.js
+++ models/chat.js
@@ -1,10 +1,18 @@
 const connection = require('./connection');
 
-const getAllMessages = async () => {
-  const messages = await connection().collections('messages').find({}).toArray();
-  return messages;
+const getAllMessages = async () => { 
+  await connection().collections('messages').find({}).toArray();
+};
+
+const saveMessage = async (message, nickname, timestamp) => {
+  await connection().collections('messages').insertOne({
+    message,
+    nickname,
+    timestamp,
+  });
 };
 
 module.exports = {
   getAllMessages,
+  saveMessage,
 };
