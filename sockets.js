441402cb81030f85df683d34c0f17ee6ad63bb18
joao966 <joaooliveira966@gmail.com>
2021-10-29 18:22:26 -0300

    ADD: request-04

--- sockets.js
+++ sockets.js
@@ -4,32 +4,26 @@ const { create, getAll } = require('./controllers/chatController');
 }; */
 const usersLogados = [];
 
-const filterUserLogados = (nameAleatorio) => {
+/* const filterUserLogados = (nameAleatorio) => {
   usersLogados.unshift(nameAleatorio);
-  console.log('nameAleatorio :', usersLogados);
-  
-  /* const result = usersLogados.map((curr) => {
-    console.log('usersLogados', usersLogados);
-    if (curr !== nameAleatorio) {
-      console.log('curr1 :', curr);
-      return nameAleatorio;
-    }
-    console.log('nameAleatorio2 :', nameAleatorio);
-    return null;
-  });
-  return result; */
-};
+}; */
 
-const salveMessage = async (message, nickname, io) => {
+const salveMessage = async (message, nickname, io, idAleatorio) => {
   /* https://stackoverflow.com/questions/18229022/how-to-show-current-time-in-javascript-in-the-format-hhmmss
   https://pt.stackoverflow.com/questions/6526/como-formatar-data-no-javascript */
+  let name;
+  if (!nickname) {
+    name = idAleatorio;
+  } else {
+    name = nickname;
+  }
   const d = new Date();
   const data = d.toISOString().substr(0, 10).split('-').reverse()
   .join('-');
   const horas = d.toLocaleTimeString();
   const timestamp = `${data} ${horas}`;
-  await create({ message, nickname, timestamp });
-  io.emit('message', `${timestamp} - ${nickname} : ${message}`);
+  await create({ message, name, timestamp });
+  io.emit('message', `${timestamp} - ${name} : ${message}`);
 };
 
 const getAllMessages = async () => {
@@ -38,41 +32,42 @@ const getAllMessages = async () => {
 };
 
 const disconnect = (id) => {
+  console.log('filter id :', usersLogados);
+  console.log('disconnect id :', id);
   const result = usersLogados.map((curr, index) => {
     if (curr === id) {
       usersLogados.splice(index, 1);
     }
     return curr;
   });
+  console.log('\n');
   return result;
 };
 
 const atualizarNome = (nickname, idAleatorio) => {
-  const result = usersLogados.map((curr, index) => {
+  usersLogados.map((curr, index) => {
     if (curr === idAleatorio) {
       usersLogados.splice(index, 1);
       usersLogados.push(nickname);
     }
     return curr;
   });
-  usersLogados.sort();
-  return result;
+  return nickname;
 };
 
 module.exports = (io) => 
-  io.on('connection', async (socket) => {
+io.on('connection', async (socket) => {
     let idAleatorio = socket.id.slice(0, 16);
-    filterUserLogados(idAleatorio);
+    usersLogados.unshift(idAleatorio);
     io.emit('nomeAleatorio', usersLogados);
     
     socket.on('salveUser', (nickname) => {
-      atualizarNome(nickname, idAleatorio);
-      idAleatorio = nickname;
+      idAleatorio = atualizarNome(nickname, idAleatorio);
       io.emit('nomeAleatorio', usersLogados);
     });
     
     socket.on('message', ({ chatMessage, nickname }) => {
-      salveMessage(chatMessage, nickname, io);
+      salveMessage(chatMessage, nickname, io, idAleatorio);
     });
     const getAlls = await getAllMessages();
 
